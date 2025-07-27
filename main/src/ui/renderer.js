a24dd1f8fb59d2d0d477774124bc2c805e3322f7
null <noreply>
2025-07-27 16:58:02 +0700

    ,

--- main/src/ui/renderer.js
+++ main/src/ui/renderer.js
@@ -1,6 +1,7 @@
 import figlet from "figlet";
 import _boxen from "boxen";
 const boxen = _boxen.default || _boxen;
+const os = require("os");
 import Table from "cli-table3";
 class UIRenderer {
 static getSystemStats() {
